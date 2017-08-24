f51ed2750b9a03489cae57dad2f38c1ac8343353
Aaron <aaronsy2007@yahoo.com>
2017-08-23 22:57:28 -0700

    Got this idea from using Semantic-UI.

--- src/components/ResultsSingle.js
+++ src/components/ResultsSingle.js
@@ -3,21 +3,20 @@ import React from 'react';
 const ResultsSingle = ({ title, link, desc}) => {
   return(
       <div>
-        Single Result:
-        <article>
-          <header>
-            {title}
-          </header>
-          <section>
-            Short Description:
-            <p>
-              {desc}
-            </p>
-          </section>
-        </article>
-        <footer>
-          <a href={link}>Link to page</a>
-        </footer>
+        <a className='results-single__link' href={link}>
+          Single Result:
+          <article>
+            <header>
+              {title}
+            </header>
+            <section>
+              Short Description:
+              <p>
+                {desc}
+              </p>
+            </section>
+          </article>
+        </a>
       </div>
   );
 };
